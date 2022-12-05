a7cf0eeb8c376f1b9fc55c2282326eae9b14abee
TrevorH08 <trevorh8700@gmail.com>
2022-12-05 06:32:48 -0800

    add TeaControl, NewTeaForm, add state and watched react videos

---
+++ src/components/TeaControl.js
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import NewTeaForm from './NewTeaForm';
+import TeaList from './TeaList';
+
+class TeaControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTeaForm />
+    } else {
+      currentlyVisibleState = <TeaList />
+    }
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TeaControl;
