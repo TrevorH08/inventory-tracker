39718052f3cf0986a76e4bf0934f20b0124fa12e
TrevorH08 <trevorh8700@gmail.com>
2022-12-05 05:01:06 -0800

    add initial images for practice and site appearances

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import whiteLotus from "./../img/whiteLotus.png";
+import jasmineTea from "./../img/jasmineTea.png";
 
 function Header(){
   return (
     <React.Fragment>
+      <img src={whiteLotus} alt="An image of a White Lotus Pai Sho tile" />
       <h1>Jade Dragon Tea</h1>
       <hr/>
+      <img src={jasmineTea} alt="An image of Jasmine Tea" />
     </React.Fragment>
   );
 }
